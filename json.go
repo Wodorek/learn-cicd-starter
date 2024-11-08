30274594e5d397767bfb0288ba37ff5cf5009ab3
Wodorek <krol.w94@gmail.com>
2024-11-08 18:41:58 +0100

    fixed gosec

--- json.go
+++ json.go
@@ -30,6 +30,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	_, err = w.Write(dat)
 
 	if err != nil {
-		return
+		log.Printf("Critical error writing response: %s", err)
 	}
 }
